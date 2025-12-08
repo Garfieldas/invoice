393956374cfc417acaa381e07853fdbb2c88f814
Garieldas <dainiusrainys99@gmail.com>
2025-12-09 00:08:54 +0200

    Created customer model

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255, blank=False)
+    last_name = models.CharField(max_length=255, blank=False)
+    company_name = models.CharField(max_length=255, blank=False)
+    company_code = models.CharField(max_length=50, unique=True, blank=False)
+    vat_code = models.CharField(max_length=50, blank=False)
+    address = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.company_name
