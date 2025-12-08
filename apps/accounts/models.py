885f7afd497fd798aadede0968a62498763cb713
Garieldas <dainiusrainys99@gmail.com>
2025-12-08 23:34:10 +0200

    Created base user manager. Added apps folder path to base DIR

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,7 +1,30 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import  AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.core.exceptions import ValidationError
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     public_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
@@ -9,7 +32,9 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = UserManager()
 
     def save(self, *args, **kwargs):
         if not self.first_name:
