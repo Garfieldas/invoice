2c53a8a0da023db06648543f70a4778c43624d18
Garieldas <dainiusrainys99@gmail.com>
2025-12-09 00:42:52 +0200

    Edited customer admin class

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -3,4 +3,5 @@ from customers.models import Customer
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("first_name", "last_name", "company_name")
+    search_fields = ('first_name', 'last_name', 'company_name', 'address', 'company_code')
