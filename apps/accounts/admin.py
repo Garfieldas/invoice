873c226a1de88fa8f9f30ffd05fa8bcb51cf595e
Garieldas <dainiusrainys99@gmail.com>
2025-12-09 00:37:11 +0200

    Created admin class for self info

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from accounts.models import User, SelfInfo
 
 @admin.register(User)
 class BaseUserAdmin(UserAdmin):
@@ -20,3 +22,11 @@ class BaseUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+@admin.register(SelfInfo)
+class SelfInfoAdmin(admin.ModelAdmin):
+    list_display = ("individual_code", "address", "user")
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+        return qs.select_related("user")
