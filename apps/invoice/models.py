0f975e17d71d154a13603160c627a1501e74a513
Garieldas <dainiusrainys99@gmail.com>
2025-12-10 22:26:26 +0200

    Created model for invoice and invoice number

--- apps/invoice/models.py
+++ apps/invoice/models.py
@@ -1,3 +1,58 @@
+from datetime import timedelta
 from django.db import models
+from django.conf import settings
+from customers.models import Customer
 
-# Create your models here.
+class Invoice(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='invoices')
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=5, decimal_places=2)
+    invoice_number = models.PositiveIntegerField(blank=False)
+    due_date = models.DateField(blank=False, help_text="Date until customer should pay")
+    created_at = models.DateField(auto_now=True)
+
+    def generate_invoice_number(self):
+        """
+        AutoIncrement invoice numbers according to the last one
+        """
+        last_invoice = (
+            Invoice.objects
+            .filter(user=self.user)
+            .order_by('-created_at')
+            .first()
+        )
+        if not last_invoice:
+            self.invoice_number = 1
+        else:
+            self.invoice_number = last_invoice.invoice_number + 1
+
+    def generate_pay_until_date(self):
+        """
+        If user did not selected due to date for invoice payment
+        Assign 15 days from creation date
+        """
+        self.pay_until = self.created_at + timedelta(days=15)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.due_date:
+            self.generate_pay_until_date()
+        self.generate_invoice_number()
+
+    def __str__(self):
+        return f"{self.user.full_name} - {self.invoice_number} - {self.created_at}"
+    
+
+class InvoiceItem(models.Model):
+    UNITS=(
+        ('HOURS', 'val'),
+        ('AMOUNT', 'vnt')
+    )
+    name = models.CharField(max_length=250, blank=False)
+    price = models.DecimalField(max_digits=3, decimal_places=2, help_text="Price per unit")
+    amount = models.PositiveIntegerField(blank=False)
+    unit = models.CharField(choices=UNITS, max_length=10, blank=False)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} - {self.unit}"
