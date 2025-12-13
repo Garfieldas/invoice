2751d62d836ab92e8e5b0534141a419265a13fd9
Garieldas <dainiusrainys99@gmail.com>
2025-12-13 23:23:49 +0200

    Fixed layout issues created self info form class and page

---
+++ apps/accounts/forms/self_info_form.py
@@ -0,0 +1,22 @@
+from django import forms
+from accounts.models import SelfInfo
+
+class SelfInfoForm(forms.ModelForm):
+    class Meta:
+        model = SelfInfo
+        fields = [
+            'individual_code',
+            'activity_start_date',
+            'address',
+            'phone_number',
+            'bank_name',
+            'iban'
+            ]
+        widgets = {
+            'individual_code': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'activity_start_date': forms.DateInput(attrs={'class': 'input input-bordered w-full', 'type': 'date'}),
+            'address': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'phone_number': forms.TextInput(attrs={'class': 'input input-bordered w-full', 'type': 'tel'}),
+            'bank_name': forms.Select(attrs={'class': 'select select-bordered w-full'}),
+            'iban': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+        }
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import dashboard
+from accounts.views import dashboard, self_info
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('settings/', self_info, name='settings'),
 ]
