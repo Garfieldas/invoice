cefc23092007f34722c4b9395d1c7bbc902fc2c5
Garieldas <dainiusrainys99@gmail.com>
2025-12-28 18:57:08 +0200

    Changed default admin url

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('garfield/administration', admin.site.urls),
     path('', include('accounts.urls')),
     path('customers/', include('customers.urls')),
     path('invoices/', include('invoice.urls')),
