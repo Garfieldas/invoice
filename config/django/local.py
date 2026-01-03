881c014521fc1116b6b44fc152513c48981b0f6a
Garieldas <dainiusrainys99@gmail.com>
2026-01-03 21:24:36 +0200

    Postgress settings

--- config/django/local.py
+++ config/django/local.py
@@ -5,3 +5,10 @@ INSTALLED_APPS += ["debug_toolbar"]
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
