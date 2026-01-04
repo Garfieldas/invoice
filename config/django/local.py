b6d15a11e59c1d9aa285bb8a327c4fb2535d45b9
Garieldas <dainiusrainys99@gmail.com>
2026-01-04 22:49:34 +0200

    Moved redis, postgress, rq worker to docker containers for development

--- config/django/local.py
+++ config/django/local.py
@@ -7,23 +7,21 @@ MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 INTERNAL_IPS = ["127.0.0.1"]
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
 RQ_QUEUES = {
-    'default': {
-        'HOST': env('RQ_DEFAULT_HOST'),
-        'PORT': env.int('RQ_DEFAULT_PORT'),
-        'DB': env.int('RQ_DEFAULT_DB'),
-        'DEFAULT_TIMEOUT': env.int('RQ_DEFAULT_TIMEOUT'),
-    },
-    'low': {
-        'HOST': env('RQ_LOW_HOST'),
-        'PORT': env.int('RQ_LOW_PORT'),
-        'DB': env.int('RQ_LOW_DB'),
-        'DEFAULT_TIMEOUT': env.int('RQ_LOW_TIMEOUT'),
+    "default": {
+        "HOST": env("RQ_DEFAULT_HOST"),
+        "PORT": env.int("RQ_DEFAULT_PORT"),
+        "DB": env.int("RQ_DEFAULT_DB"),
+        "DEFAULT_TIMEOUT": env.int("RQ_DEFAULT_TIMEOUT"),
     },
 }
--- config/django/prod.py
+++ config/django/prod.py
@@ -7,23 +7,19 @@ CSRF_TRUSTED_ORIGINS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("PGDATABASE"),
-        "USER": os.environ.get("PGUSER"),
-        "PASSWORD": os.environ.get("PGPASSWORD"),
-        "HOST": os.environ.get("PGHOST"),
-        "PORT": os.environ.get("PGPORT"),
-        "OPTIONS": {
-            "sslmode": "require",
-        },
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
 RQ_QUEUES = {
     "default": {
-        "HOST": env("REDISHOST"),
-        "PORT": env.int("REDISPORT"),
-        "DB": env.int("REDISDB"),
-        "PASSWORD": env("REDISPASSWORD"),
+        "HOST": env("RQ_DEFAULT_HOST"),
+        "PORT": env.int("RQ_DEFAULT_PORT"),
+        "DB": env.int("RQ_DEFAULT_DB"),
         "DEFAULT_TIMEOUT": env.int("RQ_DEFAULT_TIMEOUT"),
     },
 }
